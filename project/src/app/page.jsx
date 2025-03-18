afea3b1f9bf67ee7b684830fbe3a2d32ee1ecb35
plengguin <111556507+guayJheng@users.noreply.github.com>
2025-03-19 03:18:20 +0700

    Refactor : screen,font

--- project/src/app/page.jsx
+++ project/src/app/page.jsx
@@ -6,19 +6,24 @@ import { redirect } from "next/navigation";
 import React from "react";
 import Menu from "./components/Menu";
 import Calendar from "./components/Calendar";
+import "./globals.css";
+import { Noto_Sans_Thai } from "next/font/google";
 
+const notoSansThai = Noto_Sans_Thai({
+  subsets: ["thai"],
+});
 
 function mainCalendar() {
   const { data: session } = useSession();
-  // if (!session) redirect("/login");
+
   return (
-    <main>
+    <main className={`${notoSansThai.className}`}>
       <Navbar session={session} />
       <div className="flex justify-between items-stretch">
         <div className="w-[15rem] overflow-auto p-0 h-[calc(100vh-50px)]">
           <Menu />
         </div>
-        <div className="flex-1 p-6 h-sceen">
+        <div className="flex-1 p-6 h-screen">
           <Calendar />
         </div>
       </div>
